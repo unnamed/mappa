be0bfc32541a726be3022c3ab6b164260b386225
OcZi <atlas200x@gmail.com>
2022-03-06 21:03:47 -0500

    add: yaw and pitch in Vector

--- core/src/main/java/team/unnamed/mappa/object/Vector.java
+++ core/src/main/java/team/unnamed/mappa/object/Vector.java
@@ -5,7 +5,11 @@ public class Vector implements Cloneable {
     protected int y;
     protected int z;
 
-    public Vector() {}
+    protected double yaw;
+    protected double pitch;
+
+    public Vector() {
+    }
 
     public Vector(int x, int y, int z) {
         this.x = x;
@@ -20,10 +24,41 @@ public class Vector implements Cloneable {
         return this;
     }
 
+    public Vector add(int x, int y, int z, double yaw, double pitch) {
+        this.yaw += yaw;
+        this.pitch += pitch;
+        return add(x, y, z);
+    }
+
     public Vector remove(int x, int y, int z) {
         this.x -= x;
         this.y -= y;
         this.z -= z;
+        return this;
+    }
+
+    public Vector remove(int x, int y, int z, double yaw, double pitch) {
+        this.yaw -= yaw;
+        this.pitch -= pitch;
+        return remove(x, y, z);
+    }
+
+    public Vector set(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        return this;
+    }
+
+    public Vector set(int x, int y, int z, double yaw, double pitch) {
+        this.yaw = yaw;
+        this.pitch = pitch;
+        return set(x, y, z);
+    }
+
+    public Vector removeYawPitch() {
+        this.yaw = 0;
+        this.pitch = 0;
         return this;
     }
 
