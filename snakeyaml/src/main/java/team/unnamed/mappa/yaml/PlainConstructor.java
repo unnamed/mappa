d92f258d9fe67874d51043113c675f211d22c5e6
OcZi <atlas200x@gmail.com>
2022-03-09 03:09:05 -0500

    refactor: now plain constructor really make plain maps - add support for map per files

--- snakeyaml/src/main/java/team/unnamed/mappa/yaml/PlainConstructor.java
+++ snakeyaml/src/main/java/team/unnamed/mappa/yaml/PlainConstructor.java
@@ -1,43 +1,35 @@
 package team.unnamed.mappa.yaml;
 
 import org.yaml.snakeyaml.constructor.SafeConstructor;
-import org.yaml.snakeyaml.nodes.Node;
-import org.yaml.snakeyaml.nodes.Tag;
+import org.yaml.snakeyaml.nodes.*;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class PlainConstructor extends SafeConstructor {
 
-    public PlainConstructor(String prefix) {
-        this.yamlConstructors.put(Tag.MAP, new ConstructPlainMap(prefix));
+    public PlainConstructor(boolean mapPerNode) {
+        this.yamlConstructors.put(Tag.MAP, mapPerNode ? new ConstructNodePlainMap() : new ConstructAllPlainMap());
     }
 
-    public class ConstructPlainMap extends SafeConstructor.ConstructYamlMap {
-        private final String prefix;
-        private boolean first = true;
+    public abstract class ConstructPlainMap extends SafeConstructor.ConstructYamlMap {
 
-        public ConstructPlainMap(String prefix) {
-            this.prefix = prefix;
-        }
-
-        @Override
-        public Object construct(Node node) {
-            Map<String, Object> map = (Map<String, Object>) super.construct(node);
+        protected Map<String, Object> plainMap(Map<String, Object> map) {
             Map<String, Object> plainMap = new LinkedHashMap<>();
-            plainMap(first ? prefix : "", map, plainMap);
+            plainMap("", map, plainMap);
             return plainMap;
         }
 
-        public void plainMap(String path,
-                             Map<String, Object> map,
-                             Map<String, Object> toWrite) {
+        protected void plainMap(String path,
+                                Map<String, Object> map,
+                                Map<String, Object> toWrite) {
             for (Map.Entry<String, Object> entry : map.entrySet()) {
                 String key = entry.getKey();
                 Object value = entry.getValue();
-                String absolutePath = path.isEmpty() ? key : path + "." + key;
-                if (first) {
-                    first = false;
+                String absolutePath = path;
+                if (!absolutePath.isEmpty()) {
+                    absolutePath += "." + key;
+                } else {
+                    absolutePath = key;
                 }
                 if (value instanceof Map) {
                     Map<String, Object> subMap = (Map<String, Object>) value;
@@ -47,6 +39,50 @@ public class PlainConstructor extends SafeConstructor {
 
                 toWrite.put(absolutePath, value);
             }
+        }
+    }
+
+    public class ConstructNodePlainMap extends ConstructPlainMap {
+        private boolean first = true;
+
+        private final Set<String> parentNodes = new HashSet<>();
+
+        @Override
+        public Object construct(Node node) {
+            if (first) {
+                MappingNode mappingNode = (MappingNode) node;
+                List<NodeTuple> firstNodes = mappingNode.getValue();
+                for (NodeTuple nodeTuple : firstNodes) {
+                    ScalarNode scalar = (ScalarNode) nodeTuple.getKeyNode();
+                    parentNodes.add(scalar.getValue());
+                }
+
+                first = false;
+            }
+
+            Map<String, Object> map = (Map<String, Object>) super.construct(node);
+            boolean areParents = true;
+            for (String key : map.keySet()) {
+                if (!parentNodes.contains(key)) {
+                    areParents = false;
+                    break;
+                }
+            }
+
+            if (areParents) {
+                return map;
+            }
+
+            return plainMap(map);
+        }
+    }
+
+    public class ConstructAllPlainMap extends ConstructPlainMap {
+
+        @Override
+        public Object construct(Node node) {
+            Map<String, Object> map = (Map<String, Object>) super.construct(node);
+            return plainMap(map);
         }
     }
 }
