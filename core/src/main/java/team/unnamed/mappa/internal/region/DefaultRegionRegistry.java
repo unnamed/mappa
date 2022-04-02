28233427ced0c67026273acbfb75320c0ba2e5be
OcZi <atlas200x@gmail.com>
2022-04-01 23:24:27 -0500

    refactor!: reimplement RegionRegistry

--- core/src/main/java/team/unnamed/mappa/internal/region/DefaultRegionRegistry.java
+++ core/src/main/java/team/unnamed/mappa/internal/region/DefaultRegionRegistry.java
@@ -1,33 +1,64 @@
 package team.unnamed.mappa.internal.region;
 
+import team.unnamed.mappa.model.region.Cuboid;
 import team.unnamed.mappa.model.region.Region;
 import team.unnamed.mappa.model.region.RegionSelection;
+import team.unnamed.mappa.object.Chunk;
+import team.unnamed.mappa.object.ChunkCuboid;
 import team.unnamed.mappa.object.Vector;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
+@SuppressWarnings("unchecked")
 public class DefaultRegionRegistry implements RegionRegistry {
-    private final Map<String, RegionFactory> factoryMap = new HashMap<>();
-    private final Map<String, RegionSelection> selectionMap;
+    protected final Map<Class<?>, RegionFactory<?>> factoryMap = new HashMap<>();
+    protected final Map<String, Map<Class<?>, RegionSelection<?>>> selectionMap;
 
-    public DefaultRegionRegistry(Map<String, RegionSelection> selectionMap) {
+    public DefaultRegionRegistry(Map<String, Map<Class<?>, RegionSelection<?>>> selectionMap) {
         this.selectionMap = selectionMap;
+
+        // Ah shit, here we go again
+        registerRegionFactory(Vector.class, selection ->
+            new Cuboid(selection.getFirstPoint(), selection.getSecondPoint()));
+        registerRegionFactory(Chunk.class, selection ->
+            new ChunkCuboid(selection.getFirstPoint(), selection.getSecondPoint()));
     }
 
     @Override
-    public void registerRegion(String id, RegionFactory factory) {
-        factoryMap.put(id, factory);
+    public <T> void registerRegionFactory(Class<T> type, RegionFactory<T> factory) {
+        factoryMap.put(type, factory);
     }
 
     @Override
-    public Region newRegion(String id, List<Vector> points) {
-        return factoryMap.get(id).newRegion(points);
+    public <T> Region<T> newRegion(RegionSelection<T> selection) {
+        RegionFactory<T> factory = (RegionFactory<T>) factoryMap.get(selection.getType());
+        if (factory == null) {
+            return null;
+        }
+        return factory.newRegion(selection);
     }
 
     @Override
-    public RegionSelection getSelection(String id) {
-        return selectionMap.get(id);
+    public <T> RegionSelection<T> newSelection(String id, Class<T> type) {
+        RegionSelection<T> selection = RegionSelection.newSelection(type);
+        selectionMap.compute(id, (key, map) -> {
+            if (map == null) {
+                map = new HashMap<>();
+            }
+
+            map.put(selection.getType(), selection);
+            return map;
+        });
+        return selection;
+    }
+
+    @Override
+    public <T> RegionSelection<T> getSelection(String id, Class<T> type) {
+        Map<Class<?>, RegionSelection<?>> selections = selectionMap.get(id);
+        if (selections == null || selections.isEmpty()) {
+            return null;
+        }
+        return (RegionSelection<T>) selections.get(type);
     }
 }
--- core/src/main/java/team/unnamed/mappa/internal/region/RegionRegistry.java
+++ core/src/main/java/team/unnamed/mappa/internal/region/RegionRegistry.java
@@ -2,20 +2,38 @@ package team.unnamed.mappa.internal.region;
 
 import team.unnamed.mappa.model.region.Region;
 import team.unnamed.mappa.model.region.RegionSelection;
+import team.unnamed.mappa.object.Chunk;
 import team.unnamed.mappa.object.Vector;
 
-import java.util.List;
 import java.util.Map;
 
 public interface RegionRegistry {
 
-    static RegionRegistry newRegistry(Map<String, RegionSelection> selectionMap) {
+    static RegionRegistry newRegistry(Map<String, Map<Class<?>, RegionSelection<?>>> selectionMap) {
         return new DefaultRegionRegistry(selectionMap);
     }
 
-    void registerRegion(String id, RegionFactory factory);
+    <T> void registerRegionFactory(Class<T> type, RegionFactory<T> factory);
 
-    Region newRegion(String id, List<Vector> points);
+    <T> Region<T> newRegion(RegionSelection<T> selection);
 
-    RegionSelection getSelection(String id);
+    <T> RegionSelection<T> newSelection(String id, Class<T> type);
+
+    default RegionSelection<Vector> newVectorSelection(String id) {
+        return newSelection(id, Vector.class);
+    }
+
+    default RegionSelection<Chunk> newChunkSelection(String id) {
+        return newSelection(id, Chunk.class);
+    }
+
+    <T> RegionSelection<T> getSelection(String id, Class<T> type);
+
+    default RegionSelection<Vector> getVectorSelection(String id) {
+        return getSelection(id, Vector.class);
+    }
+
+    default RegionSelection<Chunk> getChunkSelection(String id) {
+        return getSelection(id, Chunk.class);
+    }
 }
