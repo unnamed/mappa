27074bf730898e67871e284828cad98322b14043
OcZi <atlas200x@gmail.com>
2022-04-02 06:02:21 -0500

    add: selection listener for region tools

---
+++ bukkit/src/main/java/team/unnamed/mappa/bukkit/listener/SelectionListener.java
@@ -0,0 +1,52 @@
+package team.unnamed.mappa.bukkit.listener;
+
+import io.github.bananapuncher714.nbteditor.NBTEditor;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import team.unnamed.mappa.bukkit.util.MappaBukkit;
+import team.unnamed.mappa.internal.region.ToolHandler;
+import team.unnamed.mappa.internal.tool.Tool;
+
+public class SelectionListener implements Listener {
+    public static final String TOOL_ID = "tool-id";
+
+    private final ToolHandler handler;
+
+    public SelectionListener(ToolHandler handler) {
+        this.handler = handler;
+    }
+
+    @EventHandler
+    public void onToolHandle(PlayerInteractEvent event) {
+        Block clickedBlock = event.getClickedBlock();
+        ItemStack item = event.getItem();
+        if (item == null || clickedBlock == null) {
+            return;
+        }
+
+        String id = NBTEditor.getString(item, TOOL_ID);
+        if (id == null) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+        Tool<Player> tool = handler.getToolById(id, player);
+        if (tool == null) {
+            return;
+        }
+
+        Action action = event.getAction();
+        Tool.Button button = MappaBukkit.toMappa(action);
+        if (button == null) {
+            return;
+        }
+
+        tool.interact(player, MappaBukkit.toMappaVector(clickedBlock), button);
+        event.setCancelled(true);
+    }
+}
