8e833bbf60fa9f51a6706999605fb802ab9edaf2
OcZi <atlas200x@gmail.com>
2022-04-01 23:26:16 -0500

    add: static method to simplfy MappaTextHandler new instance

--- core/src/main/java/team/unnamed/mappa/internal/message/MappaTextHandler.java
+++ core/src/main/java/team/unnamed/mappa/internal/message/MappaTextHandler.java
@@ -1,11 +1,18 @@
 package team.unnamed.mappa.internal.message;
 
 import me.yushust.message.MessageHandler;
+import me.yushust.message.config.ConfigurationModule;
+import me.yushust.message.source.MessageSource;
 import me.yushust.message.util.ReplacePack;
 import team.unnamed.mappa.object.TextNode;
 
 public class MappaTextHandler {
     protected final MessageHandler delegate;
+
+    public static MappaTextHandler fromSource(MessageSource source, ConfigurationModule... handles) {
+        return new MappaTextHandler(
+            MessageHandler.of(source, handles));
+    }
 
     public MappaTextHandler(MessageHandler delegate) {
         this.delegate = delegate;
