713409903dc91dca172c6abce384e6a8fa7947fb
OcZi <atlas200x@gmail.com>
2022-04-16 02:03:40 -0500

    add: SchemeMapper#resumeSessions

--- core/src/main/java/team/unnamed/mappa/internal/mapper/SchemeMapper.java
+++ core/src/main/java/team/unnamed/mappa/internal/mapper/SchemeMapper.java
@@ -7,7 +7,9 @@ import team.unnamed.mappa.throwable.ParseException;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Set;
 
 public interface SchemeMapper {
 
@@ -15,7 +17,18 @@ public interface SchemeMapper {
 
     Map<String, Object> loadSessions(MapScheme scheme, File file) throws ParseException;
 
+    default Map<String, Object> resumeSessions(Map<String, MapScheme> schemeMap, File file)
+        throws ParseException {
+        return resumeSessions(schemeMap, Collections.emptySet(), file);
+    }
+
+    Map<String, Object> resumeSessions(Map<String, MapScheme> schemeMap,
+                                       Set<String> idBlacklist,
+                                       File file) throws ParseException;
+
     void saveTo(FileWriter writer, MapSession session) throws IOException;
+
+    void serializeTo(FileWriter writer, MapSession session) throws IOException;
 
     String getFormatFile();
 }
