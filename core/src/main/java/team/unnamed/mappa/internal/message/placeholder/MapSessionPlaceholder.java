974416ea07f528f3bd1184a3d8cdf945f263b71e
OcZi <atlas200x@gmail.com>
2022-04-28 04:11:10 -0500

    add: create MapSessionPlaceholder

---
+++ core/src/main/java/team/unnamed/mappa/internal/message/placeholder/MapSessionPlaceholder.java
@@ -0,0 +1,26 @@
+package team.unnamed.mappa.internal.message.placeholder;
+
+import me.yushust.message.format.PlaceholderProvider;
+import me.yushust.message.track.ContextRepository;
+import org.jetbrains.annotations.Nullable;
+import team.unnamed.mappa.model.map.MapSession;
+
+public class MapSessionPlaceholder implements PlaceholderProvider<MapSession> {
+
+    @Override
+    public @Nullable Object replace(ContextRepository context,
+                                    MapSession session,
+                                    String placeholder) {
+        switch (placeholder) {
+            case "id":
+                return session.getId();
+            case "map_name":
+                return session.getMapName();
+            case "world_name":
+                return session.getWorldName();
+            case "scheme":
+                return session.getSchemeName();
+        }
+        return null;
+    }
+}
