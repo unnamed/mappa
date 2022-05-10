1590711d9588ee262534b406041cef913ec3b3d7
OcZi <atlas200x@gmail.com>
2022-05-10 03:37:36 -0500

    refactor(file source): change folder and filename of SCHEME_FORMAT

--- core/src/main/java/team/unnamed/mappa/internal/FileSource.java
+++ core/src/main/java/team/unnamed/mappa/internal/FileSource.java
@@ -7,17 +7,26 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public interface FileSource {
-    String SCHEME_FORMAT = "scheme-%s.%s";
+    String MAP_FORMAT = "/maps/map-%s.%s";
 
-    FileSource BASIC = (session, folder, formatFile) ->
-        new File(folder,
-            String.format(SCHEME_FORMAT,
-                session.getName(),
-                formatFile));
+    FileSource SCHEME = withFormat(MAP_FORMAT);
 
-    File file(MapScheme scheme, File folder, String formatFile);
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    static FileSource withFormat(String format) {
+        return (session, folder, extension) -> {
+            File file = new File(folder,
+                String.format(format,
+                    session.getName(),
+                    extension));
+            File parentFile = file.getParentFile();
+            parentFile.mkdirs();
+            return file;
+        };
+    }
 
-    default FileWriter fileWriter(MapScheme scheme, File folder, String formatFile) throws IOException {
-        return new FileWriter(file(scheme, folder, formatFile));
+    File file(MapScheme scheme, File folder, String extension);
+
+    default FileWriter fileWriter(MapScheme scheme, File folder, String extension) throws IOException {
+        return new FileWriter(file(scheme, folder, extension));
     }
 }
