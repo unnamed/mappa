a62a6470bc9718569081347cea0244c51afa5b6e
OcZi <atlas200x@gmail.com>
2022-05-10 03:42:06 -0500

    add(part): create MapSerializedSessionPart

---
+++ bukkit/src/main/java/team/unnamed/mappa/bukkit/command/part/MapSerializedSessionPart.java
@@ -0,0 +1,44 @@
+package team.unnamed.mappa.bukkit.command.part;
+
+import me.fixeddev.commandflow.CommandContext;
+import me.fixeddev.commandflow.exception.ArgumentParseException;
+import me.fixeddev.commandflow.part.CommandPart;
+import me.fixeddev.commandflow.stack.ArgumentStack;
+import org.jetbrains.annotations.Nullable;
+import team.unnamed.mappa.MappaBootstrap;
+import team.unnamed.mappa.bukkit.exception.ArgumentTextParseException;
+import team.unnamed.mappa.model.map.MapSerializedSession;
+import team.unnamed.mappa.object.TranslationNode;
+
+public class MapSerializedSessionPart implements CommandPart {
+    private final String name;
+    private final MappaBootstrap bootstrap;
+
+    public MapSerializedSessionPart(String name, MappaBootstrap bootstrap) {
+        this.name = name;
+        this.bootstrap = bootstrap;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void parse(CommandContext context,
+                      ArgumentStack stack,
+                      @Nullable CommandPart caller)
+        throws ArgumentParseException {
+        String id = stack.next();
+        MapSerializedSession serialized = bootstrap.getSerializedSessionById(id);
+        if (serialized == null) {
+            throw new ArgumentTextParseException(
+                TranslationNode
+                    .SERIALIZED_SESSION_NOT_FOUND
+                    .withFormal("{id}", id)
+            );
+        }
+
+        context.setValue(this, serialized);
+    }
+}
--- bukkit/src/main/java/team/unnamed/mappa/bukkit/command/part/MappaBukkitPartModule.java
+++ bukkit/src/main/java/team/unnamed/mappa/bukkit/command/part/MappaBukkitPartModule.java
@@ -4,6 +4,7 @@ import me.fixeddev.commandflow.annotated.part.AbstractModule;
 import me.fixeddev.commandflow.part.CommandPart;
 import team.unnamed.mappa.bukkit.MappaPlugin;
 import team.unnamed.mappa.internal.region.RegionRegistry;
+import team.unnamed.mappa.model.map.MapSerializedSession;
 import team.unnamed.mappa.model.map.MapSession;
 import team.unnamed.mappa.model.map.scheme.MapScheme;
 import team.unnamed.mappa.model.region.Cuboid;
@@ -30,6 +31,7 @@ public class MappaBukkitPartModule extends AbstractModule {
         bindRegistry(Vector.class, VectorPlayerPart::new);
 
         bindFactory(MapSession.class, (name, modifiers) -> new MapSessionPart(name, plugin.getBootstrap()));
+        bindFactory(MapSerializedSession.class, (name, modifiers) -> new MapSerializedSessionPart(name, plugin.getBootstrap()));
         bindFactory(MapScheme.class, (name, registry) -> new MapSchemePart(name, plugin.getBootstrap().getSchemeRegistry()));
         bindFactory(File.class, (name, modifiers) -> new FilePart(name, plugin.getDataFolder()));
     }
