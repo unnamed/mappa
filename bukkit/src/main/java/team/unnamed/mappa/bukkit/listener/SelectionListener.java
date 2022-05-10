9e3fa3950e29c242354d5f84411fa0738da81434
OcZi <atlas200x@gmail.com>
2022-05-10 03:40:27 -0500

    style(selection listener): clean code

--- bukkit/src/main/java/team/unnamed/mappa/bukkit/listener/SelectionListener.java
+++ bukkit/src/main/java/team/unnamed/mappa/bukkit/listener/SelectionListener.java
@@ -1,7 +1,6 @@
 package team.unnamed.mappa.bukkit.listener;
 
 import io.github.bananapuncher714.nbteditor.NBTEditor;
-import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,7 +9,6 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import team.unnamed.mappa.bukkit.util.MappaBukkit;
-import team.unnamed.mappa.internal.message.MappaTextHandler;
 import team.unnamed.mappa.internal.region.ToolHandler;
 import team.unnamed.mappa.internal.tool.Tool;
 
@@ -18,11 +16,9 @@ public class SelectionListener implements Listener {
     public static final String TOOL_ID = "tool-id";
 
     private final ToolHandler handler;
-    private final MappaTextHandler textHandler;
 
-    public SelectionListener(ToolHandler handler, MappaTextHandler textHandler) {
+    public SelectionListener(ToolHandler handler) {
         this.handler = handler;
-        this.textHandler = textHandler;
     }
 
     @EventHandler
@@ -43,16 +39,6 @@ public class SelectionListener implements Listener {
         if (tool == null || !player.hasPermission(tool.getPermission())) {
             return;
         }
-
-        World world = player.getWorld();
-        /*
-        if (!world.hasMetadata(MappaPlugin.SESSION_WORLD_METADATA)) {
-            textHandler.send(player,
-                BukkitTranslationNode
-                    .WORLD_NOT_FROM_SESSION
-                    .formalText());
-            return;
-        }*/
 
         Action action = event.getAction();
         Tool.Button button = MappaBukkit.toMappa(action);
