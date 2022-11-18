3ee2b94f351f403ebca8ad28d99a3b82e83ce103
salva <atlas200x@gmail.com>
2022-11-18 03:40:04 -0500

    fix(command manager): session key of MapSession doesn't use selected session

--- core/src/main/java/team/unnamed/mappa/internal/command/MappaCommandManager.java
+++ core/src/main/java/team/unnamed/mappa/internal/command/MappaCommandManager.java
@@ -4,6 +4,7 @@ import me.fixeddev.commandflow.CommandManager;
 import me.fixeddev.commandflow.ErrorHandler;
 import me.fixeddev.commandflow.annotated.part.Key;
 import me.fixeddev.commandflow.command.Command;
+import team.unnamed.mappa.internal.command.parts.Sender;
 import team.unnamed.mappa.model.map.MapSession;
 import team.unnamed.mappa.model.map.scheme.MapScheme;
 import team.unnamed.mappa.throwable.ParseException;
@@ -11,7 +12,7 @@ import team.unnamed.mappa.throwable.ParseException;
 import java.util.Map;
 
 public interface MappaCommandManager extends CommandManager {
-    Key SESSION_KEY = new Key(MapSession.class);
+    Key SESSION_KEY = new Key(MapSession.class, Sender.class);
     String MAPPA_PLAYER = "mappa player";
 
     Command registerMapScheme(MapScheme scheme) throws ParseException;
