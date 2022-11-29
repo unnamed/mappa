c4db9f754505669bbed79bb864618dbf8c1d288f
salva <atlas200x@gmail.com>
2022-11-28 20:36:47 -0500

    feat(TextUtils): add Texts#toPrettifyString with LineDeserializableList support - add Texts#spacer

--- core/src/main/java/team/unnamed/mappa/util/Texts.java
+++ core/src/main/java/team/unnamed/mappa/util/Texts.java
@@ -3,15 +3,10 @@ package team.unnamed.mappa.util;
 import me.fixeddev.commandflow.exception.CommandException;
 import net.kyori.text.Component;
 import net.kyori.text.serializer.legacy.LegacyComponentSerializer;
-import team.unnamed.mappa.model.map.property.MapCollectionProperty;
-import team.unnamed.mappa.model.map.property.MapProperty;
-import team.unnamed.mappa.object.Text;
-import team.unnamed.mappa.object.TranslationNode;
 import team.unnamed.mappa.object.config.LineDeserializable;
 import team.unnamed.mappa.object.config.LineDeserializableList;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
 public interface Texts {
@@ -41,39 +36,25 @@ public interface Texts {
         }
     }
 
-    // I hate this solution - OcZi
-    static List<Text> getActionSetTranslation(String path, MapProperty property, Object newValue) {
-        List<Text> texts = new ArrayList<>();
-        if (property instanceof MapCollectionProperty) {
-            String valueString = toPrettifyString(newValue);
-            texts.add(TranslationNode
-                .PROPERTY_LIST_ADDED
-                .withFormal("{type}", getTypeName(property.getType()),
-                    "{name}", path,
-                    "{value}", valueString
-                ));
+    static String toPrettifyString(Object o, String join) {
+        if (o instanceof LineDeserializable) {
+            LineDeserializable deserializable = (LineDeserializable) o;
+            return deserializable.deserialize();
+        } else if (o instanceof LineDeserializableList) {
+            LineDeserializableList list = (LineDeserializableList) o;
+            List<String> deserialize = list.deserialize();
+            return String.join(join, deserialize);
         } else {
-            if (newValue instanceof LineDeserializableList) {
-                texts = new ArrayList<>();
-                texts.add(TranslationNode
-                    .PROPERTY_CHANGE_TO
-                    .withFormal("{name}", path,
-                        "{value}", ""));
-                LineDeserializableList list = (LineDeserializableList) newValue;
-                for (String value : list.deserialize()) {
-                    texts.add(TranslationNode
-                        .PROPERTY_LIST_ADDED_ENTRY
-                        .with("{value}", value));
-                }
-            } else {
-                String valueString = toPrettifyString(newValue);
-                texts.add(TranslationNode
-                    .PROPERTY_CHANGE_TO
-                    .withFormal("{name}", path,
-                        "{value}", valueString));
-            }
+            return String.valueOf(o);
         }
-        return texts;
+    }
+
+    static String spacer(int spaces) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < spaces; i++) {
+            builder.append(' ');
+        }
+        return builder.toString();
     }
 
     static String capitalize(String str) {
